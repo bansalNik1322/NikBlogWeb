e536b99dc3530e66e2d7f349d454666035700048
Nikhil Bansal <103099655+bansalNik1322@users.noreply.github.com>
2022-06-13 10:47:45 +0530

    Add files via upload

---
+++ routes/route.js
@@ -0,0 +1,19 @@
+import express from "express"
+
+const router = express.Router()
+
+router.get("/", (req, res)=>{
+    res.render("home")
+})
+
+router.get("/register", (req, res)=>{
+    res.render("register")
+})
+
+router.get("/login", (req, res)=>{
+    res.render("login")
+})
+
+
+
+export default router
