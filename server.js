b716f6d86bd37d32043bfbee46a94459cd2e6445
Nikhil Bansal <103099655+bansalNik1322@users.noreply.github.com>
2022-07-20 21:52:01 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,25 +1,62 @@
-// importing npm modules --
-import express from 'express'
-import bodyParser from "body-parser"
-import ejs from "ejs"
+/**
+ * Importing Modules -- 
+ */
+import dotenv from "dotenv"
+import express from "express"
+import http from "http"
+import cookieParser from "cookie-parser"
+import session from "express-session"
+import MongoStore from "connect-mongodb-session";
+let monStore = MongoStore(session);
+let app = express()
+
+const store = new monStore({
+    uri: "mongodb://localhost:27017/NikBlog",
+    collection: "mysessions",
+});
+
+app.use(session({
+    secret: "key",
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+}))
 
 
-
-// importing user-created module
-import router from "./routes/route.js"
-
-// Initializing app 
-const app = express()
+dotenv.config()
+/**
+ * Setting Up app and predefined middleware -- 
+ */
 app.set('view engine', 'ejs');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use(express.static("public"));
-app.use(bodyParser.json())
 
-// Making Request
+/**
+ * Importing Routes -- 
+ */
 
-app.use("/", router)
+import userRoute from "./routes/userRoutes.js"
+import adminRoute from "./routes/adminRoute.js"
+app.use("/admin", adminRoute)
+app.use("/user", userRoute)
 
-// listening on port
-app.listen(3000, () => {
-    console.log(`Server is running at http://localhost:3000/`);
-})
+/**
+ * Connecting DB --
+ */
+import connectDB from "./config/connection.js"
+connectDB()
+
+
+/**
+ * Setting Up Server
+ */
+
+var server = http.createServer(app);
+let port = 3000
+server.listen(port, ()=>{
+    console.log("Server running");
+});
